5df5f5f7b50fdcc44eddd9002807cbcfba07b468
roflmaostc <f.wechsler@tum.de>
2018-01-19 00:14:10 +0100

    README and working code

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a visualization of the [Quadtree](https://en.wikipedia.org/wiki/Quadtree) using OCaml.
+
+To compile this visualization use:
+
+```bash
+ocamlbuild -package graphics quadtree.native
+```
+As you can see we use the [Graphics package](https://caml.inria.fr/pub/docs/manual-ocaml/libref/Graphics.html#VALdraw_circle) for visualization.
+
+Execution of the command can be done with:
+```bash
+./quadtree.native
+```
+
+Then a graphics window will open. With the key r you can randomly generate a new quadtree. Using x you can close the window
--- quadtree.ml
+++ quadtree.ml
@@ -1,7 +1,6 @@
 
 type 'a cell = {x:float; y:float; content: 'a}
 
-
 type 'a quadtree = Empty | Cell of ('a cell) | Node of {x: float; y:float; ru: 'a quadtree; lu: 'a quadtree; ld: 'a quadtree; rd: 'a quadtree}
 
 type quadrant = RightUp | LeftUp | LeftDown | RightDown
@@ -25,30 +24,51 @@ let rec insert cell qt size =
            lu = (if determine_quadrant x y cell.x cell.y = LeftUp    then aux2 (-.) (+.) lu else lu);
            ld = (if determine_quadrant x y cell.x cell.y = LeftDown  then aux2 (-.) (-.) ld else ld);
            rd = (if determine_quadrant x y cell.x cell.y = RightDown then aux2 (+.) (-.) rd else rd);}
-    | Cell cell2 ->
-      (* let q_cell, q_cell2 = determine_quadrant xq yq cell.x cell.y, determine_quadrant xq yq cell2.x cell2.y *) 
-      (* in *)
-      (* if q_cell <> q_cell2 then *) 
-        (* let x_new = ((if q_cell2 = RightUp || q_cell2 = RightDown then (+.) else (-.)) xqt (size/.2.0/.2.0**(float_of_int level))) *)
-        (* in *)
-        (* let y_new =  ((if q_cell2 = LeftUp || q_cell2 = LeftDown then (+.) else (-.)) yqt (size/.2.0/.2.0**(float_of_int level))) *) 
-        (* in *)
-        aux xqt yqt (aux xqt yqt (Node {x=xqt; y=yqt; ru=Empty; lu=Empty; ld=Empty; rd=Empty}) level cell2) level cell
-      (* else *)
-        (* aux x_new y_new (Node{x=x_new; y=y_new; ru=Empty; lu=Empty; ld=Empty; rd=Empty}) (level+1) *)
-      (* else if q_cell = RightUp then aux Node{x;y } hier eine cell ablegen im richtigen und auf diesen subpart wieder insertion machen) *)
+    | Cell cell2 -> aux xqt yqt (aux xqt yqt (Node {x=xqt; y=yqt; ru=Empty; lu=Empty; ld=Empty; rd=Empty}) level cell2) level cell
   in
   aux (size/.2.0) (size/.2.0) qt 1 cell
 
 
+let generate_random_tree number_of_points = 
+  let () = Random.self_init ()
+  in
+  let size = 1.0
+  in
+  let rec aux counter qt = 
+    if counter< number_of_points 
+    then aux (counter+1 ) (insert ({x=Random.float 1.0;y=Random.float 1.0; content=" "}) qt size)
+    else qt
+  in
+  aux 0 Empty
 
 
-let rec draw_quadtree qt scaling = 
-  Graphics.open_graph " 800x600";
-  match qt with
-  | Empty -> ()
-  | Cell c -> Graphics.fill_circle (int_of_float (c.x*.scaling) ) (int_of_float (c.y*.scaling) ) 10;
-  ignore (Graphics.read_key ())
+let rec draw_quadtree qt x_size y_size scaling offset = 
+  let offset, scaling= float_of_int offset, float_of_int scaling
+  in
+  let open Graphics in
+  Graphics.open_graph (" "^(string_of_int x_size)^"x"^(string_of_int y_size));
+  let radius = 2
+  in
+  let rec aux qt level =  
+    match qt with
+    | Empty -> ()
+    | Cell c -> set_color red; fill_circle (int_of_float (offset+.c.x*.scaling) ) (int_of_float (offset+.c.y*.scaling) ) radius; set_color black;
+    | Node n -> 
+      draw_rect (int_of_float (offset+.(n.x-.1.0/.2.0**level)*.scaling)) (int_of_float (offset+.(n.y-.1.0/.2.0**level)*.scaling)) (int_of_float (scaling*.1.0/.2.0**level)) (int_of_float (scaling*.1.0/.2.0**level));
+      draw_rect (int_of_float (offset+.(n.x)*.scaling)) (int_of_float (offset+.(n.y-.1.0/.2.0**level)*.scaling)) (int_of_float (scaling*.1.0/.2.0**level)) (int_of_float (scaling*.1.0/.2.0**level));
+      draw_rect (int_of_float (offset+.(n.x)*.scaling)) (int_of_float (offset+.(n.y)*.scaling)) (int_of_float (scaling*.1.0/.2.0**level)) (int_of_float (scaling*.1.0/.2.0**level));
+      draw_rect (int_of_float (offset+.(n.x-.1.0/.2.0**level)*.scaling)) (int_of_float (offset+.(n.y)*.scaling)) (int_of_float (scaling*.1.0/.2.0**level)) (int_of_float (scaling*.1.0/.2.0**level));
+      aux n.ru (level+.1.0); aux n.lu (level+.1.0); aux n.ld (level+.1.0); aux n.rd (level+.1.0);         
+  in
+  let () = aux qt 1.0
+  in
+  let rec refresh x = 
+    let c = read_key ()
+    in
+    if c = 'r' then let () = clear_graph () in aux (generate_random_tree 30) 1.0; refresh x
+    else if c = 'x' then ()
+    else refresh ()
+  in
+  refresh ()
 
-
-let () = draw_quadtree (Cell{x=0.5;y=0.5; content=" "}) 600.0
+let () = draw_quadtree (generate_random_tree 30) 500 500 400 30 
